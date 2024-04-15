326f4d3653c0b11476d08258726647fcc321443e
Daniel Medina <daniel.m@selia.co>
2024-04-15 12:41:31 -0400

    updated model url

--- app.py
+++ app.py
@@ -77,6 +77,7 @@ def query_huggingface_model(payload):
     :param payload: The data to send in the request.
     :return: The JSON response from the API.
     """
+    print('MODEL_API_URL:', MODEL_API_URL)
     model_headers = {
         "Accept": "application/json",
         "Authorization": "Bearer " + MODEL_API_KEY,
@@ -115,13 +116,13 @@ def process_request():
     # Extract message from the request
     message = request.json.get("message", "")
 
-    payload = {
+    emb_payload = {
 	"inputs": message,
 	"parameters": {}
     }
 
     # Step 1: Get embedding
-    embedding = embed_message(payload)
+    embedding = embed_message(emb_payload)
 
     # Step 2: Retrieve similar chunks from MongoDB
     similar_chunks = find_similar_chunks(embedding)
@@ -129,13 +130,17 @@ def process_request():
     # Step 3: Call HuggingFace model
     if similar_chunks:
         # Assuming we use the text from the first similar chunk
-        model_input = similar_chunks[0] + " " + message
-        print('model input:', model_input)
-        huggingface_response = query_huggingface_model({
-                                                        "inputs": model_input,
-                                                        "temp": 0.6
-                                                        })
+        model_payload = {
+            "inputs": "",
+            "question": message,
+            "context": similar_chunks[0],
+            "temp": 0.3,
+            "max_tokens": 250
+            }
+        huggingface_response = query_huggingface_model(model_payload)
         print('huggingface response:', huggingface_response)
+        if "error" in huggingface_response:
+            return jsonify({"message": "An error occurred while processing the request. Please try again in a few seconds."}), 200
         text_response = extract_response(huggingface_response)
         print('text response:', text_response)
         return jsonify(huggingface_response), 200
